5c852e99b83c4041b842c85ee510edf5c6806d18
steva1312 <stevanr2005@gmail.com>
2021-11-26 11:00:19 +0100

    Najjace

--- index.js
+++ index.js
@@ -2,13 +2,51 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('<h1>NAAAAJavaaaa GASCCINAAA</h1>')
+app.use(express.json())
+
+const todos = [
+    {
+        task: "Uradi domaci",
+        isDone: false,
+        id: 0
+    },
+    {
+        task: "Idi na plivanje",
+        isDone: false,
+        id: 1
+    },
+    {
+        task: "Reci dabome",
+        isDone: true,
+        id: 2
+    },
+]
+
+app.get('/api/todos', (req, res) => {
+    res.send(todos)
 })
 
-app.get('/test/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(req.params.id)
+app.get('/api/todos/:id', (req, res) => {
+    res.send(todos[req.params.id])
+})
+
+app.post('/api/todos', (req, res) => {
+    todos.push({...req.body})
+    res.send(todos)
+})
+
+app.put('/api/todos/:id', (req, res) => {
+    todos[req.params.id].isDone = !todos[req.params.id].isDone
+    res.send(todos[req.params.id])
+})
+
+app.delete('/api/todos', (req, res) => {
+    todos.map((todo, i) => {
+        if(todo.isDone) {
+            todos.splice(i, 1)
+        }
+    })
+    res.send(todos)
 })
 
 const PORT = process.env.PORT || 3333
